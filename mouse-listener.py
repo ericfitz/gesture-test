3b967e352d56b3eb5a68daf2017b01e851983bda
Eric Fitzgerald <admin@efitz.net>
2023-03-11 15:18:02 -0500

    Tap vs long tap

--- mouse-listener.py
+++ mouse-listener.py
@@ -1,11 +1,13 @@
 from pynput import mouse
 from datetime import datetime
 
-tap_duration_threshold = 100  # ms
-move_threshold = 1  # pixels
+tap_duration_sensitivity = 100  # ms
+movement_sensitivity = 1  # pixels
+longtap_sensitivity = 1000  # ms
 lastx = lasty = 0
-inGesture = False
-gesture = "none"
+inGesture = False  # True if a gesture is in progress
+gesture = "none"  # Detected gesture name
+
 
 def on_move(x, y):
     # print(f'Pointer moved to {x}, {y}')
@@ -16,23 +18,35 @@ def on_move(x, y):
     xmag = ymag = 0
     xdir = ydir = gesture = "none"
     if inGesture:
-        xmag = abs(mdeltax) if abs(mdeltax) > move_threshold else 0
+        xmag = abs(mdeltax) if abs(mdeltax) > movement_sensitivity else 0
         if xmag > 0:
             xdir = "right" if mdeltax > 0 else "left"
-        ymag = abs(mdeltay) if abs(mdeltay) > move_threshold else 0
+        ymag = abs(mdeltay) if abs(mdeltay) > movement_sensitivity else 0
         if ymag > 0:
-            ydir = "down" if mdeltay > 0 else "up" # y-axis is inverted vs. cartesian
+            ydir = "down" if mdeltay > 0 else "up"  # y-axis is inverted vs. cartesian
         if ydir == "none":
-            gesture = "tap" if xdir == "none" else "swipe-right" if xdir == "right" else "swipe-left"
+            gesture = (
+                "tap"
+                if xdir == "none"
+                else "swipe-right"
+                if xdir == "right"
+                else "swipe-left"
+            )
         if xdir == "none" and gesture == "none":
-            gesture = "tap" if ydir == "none" else "swipe-up" if ydir == "up" else "swipe-down"
+            gesture = (
+                "tap"
+                if ydir == "none"
+                else "swipe-up"
+                if ydir == "up"
+                else "swipe-down"
+            )
         # print(f'Pointer moved by {mdeltax}, {mdeltay}; direction: {xdir}, {ydir}')
-        print(f'Gesture: {gesture}')
+        print(f"Gesture: {gesture}")
     pass
 
 
 def on_click(x, y, button, pressed):
-    global presstime, releasetime, startx, starty, deltax, deltay, deltats, deltatms, inGesture
+    global presstime, releasetime, startx, starty, deltax, deltay, deltats, deltatms, inGesture, gesture
     presstext = "pressed" if pressed else "released"
     print(f"{button} {presstext} at {x}, {y}")
     if pressed:
@@ -46,8 +60,12 @@ def on_click(x, y, button, pressed):
         deltax, deltay = x - startx, y - starty
         deltats = (releasetime - presstime).seconds * 1000
         deltatms = (releasetime - presstime).microseconds // 1000 + deltats
-        tapgesture = "tap" if deltatms < tap_duration_threshold else gesture
-        print(f"Movement vector {deltax}, {deltay} in {deltatms} ms; Gesture: {gesture}")
+        if gesture == "tap":
+            if deltatms > longtap_sensitivity:
+                gesture = "longtap"
+        print(
+            f"Movement vector {deltax}, {deltay} in {deltatms} ms; Gesture: {gesture}"
+        )
         pass
 
 
