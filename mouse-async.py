3497c5430a8cd776a09f57dc187bc816d7c0b56a
Eric Fitzgerald <admin@efitz.net>
2023-05-30 19:18:12 -0400

    Fixed 100% CPU usage issue

--- mouse-async.py
+++ mouse-async.py
@@ -1,28 +1,31 @@
-# monitoring this demo during execution showed that it kept one thread at 100% cpu usage
 from pynput import mouse
+import time
 
 
 def on_move(x, y):
-    print('Pointer moved to {0}'.format(
-        (x, y)))
+    print("Pointer moved to {0}".format((x, y)))
 
 
 def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
+    print("{0} at {1}".format("Pressed" if pressed else "Released", (x, y)))
     if not pressed:
         # Stop listener
         # return False
         pass
 
+
 def main():
-    listener = mouse.Listener(
-        on_move=on_move,
-        on_click=on_click)
+    listener = mouse.Listener(on_move=on_move, on_click=on_click)
     listener.start()
-    while True:
-        pass
+    print("Listener started")
+    try:
+        while True:
+            time.sleep(0.1)
+            pass
+    except KeyboardInterrupt:
+        listener.stop()
+        print("Listener stopped")
+        exit(0)
 
 
 if __name__ == "__main__":
